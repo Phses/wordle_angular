bad6d5a1eff60a34b50e1bc04eab28ab8b7a5547
Phses <phsouzaesilva@gmail.com>
2023-03-09 21:05:27 -0300

    add unsbscribe as inscrições no momento em que o component e destruido

--- src/app/base/board/board.component.ts
+++ src/app/base/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { statusLetra } from 'src/app/enums';
+import { Subscription } from 'rxjs';
 import { BaseService } from '../base.service';
 
 @Component({
@@ -8,18 +9,21 @@ import { BaseService } from '../base.service';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent {
+  letraDigitadaSubscription: Subscription = new Subscription
+  letraApagadaSubscription: Subscription = new Subscription
+  verificaPalavraChuteSubscription: Subscription = new Subscription
   constructor(private baseService: BaseService, private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    this.baseService.letraDigitada.subscribe((chute) => {
+    this.letraDigitadaSubscription = this.baseService.letraDigitada.subscribe((chute) => {
       const campo = this.elementRef.nativeElement.querySelector(`.tentativa-${chute.tentativaAtual} .campo-${chute.campoAtual}`);
       this.renderer.setProperty(campo, 'textContent', chute.letra);
     })
-    this.baseService.letraApagada.subscribe((chute) => {
+    this.letraApagadaSubscription = this.baseService.letraApagada.subscribe((chute) => {
       const campo = this.elementRef.nativeElement.querySelector(`.tentativa-${chute.tentativaAtual} .campo-${chute.campoAtual}`);
       this.renderer.setProperty(campo, 'textContent', "");
     })
-    this.baseService.verificaPalavraChute.subscribe((verificacao) => {
+    this.verificaPalavraChuteSubscription = this.baseService.verificaPalavraChute.subscribe((verificacao) => {
       for(let i = 0; i <= 6; i++) {
         const campo = this.elementRef.nativeElement.querySelector(`.tentativa-${this.baseService.chute.tentativaAtual} .campo-${i + 1}`)
         switch(verificacao[i]) {
@@ -35,5 +39,10 @@ export class BoardComponent {
         }
       }
     })
+  }
+  ngOnDestroy() {
+    this.letraDigitadaSubscription.unsubscribe();
+    this.letraApagadaSubscription.unsubscribe();
+    this.verificaPalavraChuteSubscription.unsubscribe();
   }
 }
