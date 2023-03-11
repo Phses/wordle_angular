b775f17e8be644c7fa3f608efc4fff00813728fa
Phses <phsouzaesilva@gmail.com>
2023-03-11 06:20:47 -0300

    add background para as teclas de acordo com o status da letra chutada

--- src/app/base/teclado/teclado.component.ts
+++ src/app/base/teclado/teclado.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { statusLetra } from 'src/app/enums';
 import { BaseService } from '../base.service';
 
 @Component({
@@ -8,8 +10,37 @@ import { BaseService } from '../base.service';
 })
 export class TecladoComponent {
 
-  constructor(private baseService: BaseService) {}
+  verificaPalavraChuteSubscription: Subscription = new Subscription
+  letrasJaChutadas: string[] = []
 
+  constructor(private baseService: BaseService, private elementRef: ElementRef, private renderer: Renderer2) {}
+  
+  ngOnInit() {
+    
+    this.verificaPalavraChuteSubscription = this.baseService.verificaPalavraChute.subscribe((verificacao) => {
+      for(let i = 0; i < this.baseService.palavraChute.length; i++) {
+        console.log(this.baseService.palavraChute[i].toLocaleLowerCase())
+        const letra: string = this.baseService.palavraChute[i].toLocaleLowerCase()
+        if(this.letrasJaChutadas.find(x => x === letra)) {
+          return
+        }
+        this.letrasJaChutadas.push(letra)
+        const tecla = this.elementRef.nativeElement.querySelector(`.${letra}`)
+        console.log(tecla)
+        switch(verificacao[i]) {
+          case statusLetra.PosicaoCerta:
+            this.renderer.addClass(tecla, 'letraExiste')
+            break;
+          case statusLetra.PosicaoErrada:
+            this.renderer.addClass(tecla, 'letraExiste')
+            break;
+          case statusLetra.NaoExiste:
+            this.renderer.addClass(tecla, 'naoExiste');
+            break;
+        }
+      }
+    })
+  }
   onClick(letraChute: string): void {
     console.log(letraChute)
     this.baseService.addLetraChute(letraChute)
